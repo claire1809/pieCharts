288748d315493b4c357ddb191c78cfbc2f7ebffa
wenjing.shi <wenjing.shi@bluefocus.com>
2020-11-18 14:14:28 +0800

    v1 双击饼图穿透；柱状图上的日期不可查询；

---
+++ server.js
@@ -0,0 +1,350 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const querystring = require("querystring");
+const http = require('http');
+const https = require('https');
+const views = require("koa-views");
+const static = require("koa-static");
+const { resolve } = require("path");
+const { rejects } = require("assert");
+// var iconv = require('iconv-lite');
+
+
+
+
+const corpid = 'wx44673e3489d0f215';
+const corpsecret = 'nu2uk2ylWLS-341eD2xUBg0dqlePBpORJtFfQglxykA';
+// const requestUri = 'http://oatest.bluefocusgroup.com:8089/mobile/plugin/jsp/chart/';//测试
+const requestUri = 'http://oa.bluefocusgroup.com:8088/mobile/plugin/jsp/chart/';//正式
+
+
+let app = new Koa();
+let router = new Router();
+
+// /static --->locahost:8888
+app.use(static(__dirname + "/static"));
+app.use(views(__dirname + "/views",{
+    // extension:"pug"
+    map:{
+        html:"pug"
+    }
+}));
+
+router.get('/wechat/qy',async ctx => {
+    // console.log(ctx.query.code);
+    let code = ctx.query.code;
+    // console.log('code=' + code);
+    try {
+        let token = await getAccessToken();
+        let userid = await getUserInfo(token, code);
+        ctx.redirect('/menu?userid=' + userid);
+    } catch(error) {
+        console.log('处理失败的promise：' + error);
+        ctx.body = "出现异常！";
+    }
+});
+
+router.get('/menu', async (ctx) => {
+    const getMenuUrl = "getMenuInfo.jsp";
+    let userid = ctx.query.userid;
+    const postData = querystring.stringify({
+        'userid' : userid
+    });
+    let menus = await getData(getMenuUrl, postData);
+    if(menus.length === 0) {
+        ctx.body = "请联系管理员开通权限！";
+    } else {
+        await ctx.render('menu.pug', {'userid':userid,menus});
+    }
+});
+
+router.get('/ReceivableRatio',async (ctx)=>{
+    // console.log(ctx.query.id);
+    
+    let {id, userid, authid} = ctx.query;
+    let auth = {};
+    // console.log("authid",authid);
+    // console.log(typeof(authid))
+    if(typeof(authid) == "undefined") {
+        // console.log("first");
+        const authUrl = 'getUserAuth.jsp';
+        const postData1 = querystring.stringify({
+            'userid' : userid,
+            'menuID' : id
+        });
+        // console.log(postData1);
+        auth = await getData(authUrl, postData1);
+    } else {
+        // console.log("second");
+        auth = [{
+            'AUTH_TYPE': '0',
+            'AUTH_ID': authid
+        }];
+    }
+    // console.log("auth");
+    // console.log(auth);
+    
+    await ctx.render('ReceivableRatio/index.pug',{auth});
+    
+})
+
+router.get('/getData', async (ctx) => {
+    // console.log("getData");
+    // console.log(ctx.query);
+    let {nowTime, authType, authID} = ctx.query;
+    let postData = querystring.stringify({
+        'authType': authType,
+        'authID': authID,
+        'getType': 'now',
+        'season': nowTime
+    });
+    let postData1 = querystring.stringify({
+        'authType': authType,
+        'authID': authID,
+        'getType': 'lastMonth',
+        'season': nowTime
+    });
+    const getDataUrl = 'receiveRatio/getByHat.jsp';
+    let nowData = await getData(getDataUrl, postData);
+    console.log(postData);
+    let lastMonthData = await getData(getDataUrl, postData1);
+    console.log(nowData);
+
+    // let data = {
+    //     name: 'claire'
+    // }
+    ctx.body = {nowData,lastMonthData};
+});
+
+
+
+router.get('/ReceivableRatio/detail', async (ctx) => {
+    // console.log("==========================detail");
+    // console.log(ctx.query);
+    let hatid = ctx.query.companyid;
+    await ctx.render('ReceivableRatio/detail.pug',{hatid});
+});
+
+router.get('/getData/detail', async ctx => {
+    // console.log('/getData/detail');
+    // console.log(ctx.query);
+    let {type, nowTime, hatid} = ctx.query;
+    let getDataUrl = "";
+    if(type === "client") {
+        getDataUrl = 'receiveRatio/getClientData.jsp';
+    } else if (type === "dept") {
+        getDataUrl = 'receiveRatio/getDeptData.jsp';
+    }
+    let postData = querystring.stringify({
+        'hatID': hatid,
+        'getType': 'now',
+        'season': nowTime
+    });
+    let postData1 = querystring.stringify({
+        'hatID': hatid,
+        'getType': 'lastMonth',
+        'season': nowTime
+    });
+    let nowData = await getData(getDataUrl, postData);
+    // console.log(nowData);
+    // console.log("+++++++++++++++++++++++++++++++++++++++++");
+    let lastMonthData = await getData(getDataUrl, postData1);
+    console.log(lastMonthData);
+    ctx.body = {nowData,lastMonthData};
+})
+
+
+
+
+
+
+
+
+function getUserInfo(token, code) {
+    return new Promise((resolve, rejects) => {
+        // console.log('code=' + code);
+        // console.log('token=' + token);
+        let getUserInfoUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=" + token + "&code=" + code;
+        https.get(getUserInfoUrl, res => {
+            // console.log(res.headers);
+            // console.log('获取用户信息')
+            let userData = "";
+            res.on('data', chunck => {
+                // console.log('getUserInfo-data方法');
+                // console.log(chunck);
+                // process.stdout.write(chunck);
+                userData = JSON.parse(chunck);
+                console.log(userData);
+                
+            });
+
+            res.on('end', () => {
+                if(userData['errcode'] === 0) {
+                    userid = userData['UserId'];
+                    resolve(userid);
+                } else {
+                    rejects();
+                }
+            });
+        });
+    });
+}
+
+
+
+function getData(urlString, postData) {
+    return new Promise((resolve, rejects) => {
+        const url = requestUri + urlString;
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            }
+        }
+        // console.log(url);
+        let result = '';
+        let req = http.request(url, options, (res) => {
+            // // res.setEncoding('utf8');
+            // console.log(res.headers);
+            // console.log(res.chunck);
+            res.on('data', chunck => {
+                // console.log('getUserAuth-data方法');
+                // console.log(JSON.parse(chunck));
+                // console.log("chunck", chunck);
+                result += chunck;
+                // console.log("result", result);
+            });
+
+            res.on('end', () => {
+                // console.log(result);
+                result = JSON.parse(result);
+                // process.stdout.write(chunck);
+                // resData = JSON.parse(chunck);
+                // console.log(result);
+                resolve(result);
+            })
+        })
+        req.on('error', (e) => {
+            console.error(`请求遇到问题: ${e.message}`);
+            rejects("error");
+        });
+        req.write(postData);
+        req.end();
+    });
+}
+
+function getMenu(userid) {
+    return new Promise((resolve, rejects) => {
+        console.log("getMenu");
+        const url = "http://oatest.bluefocusgroup.com:8089/mobile/plugin/jsp/chart/getMenuInfo.jsp";
+        const postData = querystring.stringify({
+            'userid' : userid
+        });
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            }
+        }
+
+        
+        let menus = '';
+        let req = http.request(url, options, (res) => {
+            res.on('data', chunck => {
+                // console.log('getUserAuth-data方法');
+                // console.log(JSON.parse(chunck));
+                menus = JSON.parse(chunck);
+                // process.stdout.write(chunck);
+                // resData = JSON.parse(chunck);
+                // console.log(resData);
+                resolve(menus);
+            });
+
+        })
+        req.on('error', (e) => {
+            console.error(`请求遇到问题: ${e.message}`);
+        });
+        req.write(postData);
+        req.end();
+        
+    })
+}
+
+function getUserAuth() {
+    return new Promise((resolve, rejects) => {
+        // console.log("getUserAuth");
+        const url = "http://oatest.bluefocusgroup.com:8089/mobile/plugin/jsp/chart/getUserAuth.jsp";
+        const postData = querystring.stringify({
+            'userid' : userid
+        });
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            }
+        }
+
+        
+
+        let req = http.request(url, options, (res) => {
+            res.on('data', chunck => {
+                // console.log('getUserAuth-data方法');
+                // console.log(JSON.parse(chunck));
+                // process.stdout.write(chunck);
+                // resData = JSON.parse(chunck);
+                // console.log(resData);
+                resolve(JSON.parse(chunck));
+            });
+
+        })
+        req.on('error', (e) => {
+            console.error(`请求遇到问题: ${e.message}`);
+        });
+        req.write(postData);
+        req.end();
+    })
+}
+
+
+
+
+function getAccessToken() {
+    return new Promise((resolve, rejects) => {
+        let url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + corpsecret;
+        let resData = "";
+        https.get(url, res => {
+            // console.log(res.headers);
+            
+            res.on('data', chunck => {
+                console.log('getAccessToken-data方法');
+                // console.log(chunck);
+                // process.stdout.write(chunck);
+                resData = JSON.parse(chunck);
+                // console.log(resData);
+                
+            });
+
+            res.on('end', () => {
+                // console.log('getAccessToken-end方法');
+                // console.log(resData);
+                if(resData['errcode'] === 0) {
+                    let token = resData['access_token'];
+                    console.log('endtoken',token);
+                    resolve(token);
+                } else {
+                    rejects(resData['errmsg']);
+                }
+            })
+            
+        });
+    })
+    
+    // return resData;
+}
+
+app.use(router.routes());
+ 
+app.listen(8888);
---
+++ static/js/common.js
@@ -0,0 +1,98 @@
+/**
+ * 金额格式化
+ * 以万为单位，保留2位小数
+ * @param {金额} num 
+ */
+function formatMoney(num) {
+    let money = Number((num / 10000).toFixedBfc(2));
+    return money.toLocaleString('zh', {
+        // style: 'currency',
+        // currency: "CNY",
+        // notation: 'compact',
+        // compactDisplay: 'short',
+        // maximumFractionDigits: 2
+    }) + '万';
+}
+
+/**
+ * 获取上升或下降符号
+ * @param {当前金额} num1 
+ * @param {上周或上月金额} num2 
+ */
+function getNotation(num1, num2) {
+    let htmlStr = "";
+    if(num1 < num2) {
+        htmlStr = `<span class='flag flag-down'>↓</span>`;
+    } else if (num1 > num2) {
+        htmlStr = `<span class='flag flag-up'>↑</span>`;
+    }
+    return htmlStr;
+}
+
+//toFixed兼容方法，返回值为String类型！！！
+Number.prototype.toFixedBfc = function(len){
+    if(len>20 || len<0){
+        throw new RangeError('toFixed() digits argument must be between 0 and 20');
+    }
+    if(typeof len === "undefined") {
+        len = 2;
+    }
+    // console.log("toFixedBfc:",len)
+    // .123转为0.123
+    var number = Number(this);
+    if (isNaN(number) || number >= Math.pow(10, 21)) {
+        return number.toString();
+    }
+    if (typeof (len) == 'undefined' || len == 0) {
+        return (Math.round(number)).toString();
+    }
+    var result = number.toString(),
+        numberArr = result.split('.');
+
+    if(numberArr.length<2){
+        //整数的情况
+        return padNum(result);
+    }
+    var intNum = numberArr[0], //整数部分
+        deciNum = numberArr[1],//小数部分
+        lastNum = deciNum.substr(len, 1);//最后一个数字
+    
+    if(deciNum.length == len){
+        //需要截取的长度等于当前长度
+        return result;
+    }
+    if(deciNum.length < len){
+        //需要截取的长度大于当前长度 1.3.toFixed(2)
+        return padNum(result)
+    }
+    //需要截取的长度小于当前长度，需要判断最后一位数字
+    result = intNum + '.' + deciNum.substr(0, len);
+    if(parseInt(lastNum, 10)>=5){
+        //最后一位数字大于5，要进位
+        var times = Math.pow(10, len); //需要放大的倍数
+        var changedInt = Number(result.replace('.',''));//截取后转为整数
+        changedInt++;//整数进位
+        changedInt /= times;//整数转为小数，注：有可能还是整数
+        result = padNum(changedInt+'');
+    }
+    return result;
+    //对数字末尾加0
+    function padNum(num){
+        var dotPos = num.indexOf('.');
+        if(dotPos === -1){
+            //整数的情况
+            num += '.';
+            for(var i = 0;i<len;i++){
+                num += '0';
+            }
+            return num;
+        } else {
+            //小数的情况
+            var need = len - (num.length - dotPos - 1);
+            for(var j = 0;j<need;j++){
+                num += '0';
+            }
+            return num;
+        }
+    }
+}
