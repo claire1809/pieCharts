b0af28ce0e2cbd3556987f4d11581f5044e8cfa7
wenjing.shi <wenjing.shi@bluefocus.com>
2020-12-28 17:09:00 +0800

    增加财务应收比例报表展示

--- router.js
+++ router.js
@@ -3,6 +3,6 @@ const financeRatioRouter = require("./routers/financeRatioRouter");
 const mainRouter = require("./routers/mainRouter");
 module.exports = function(app){
     app.use(receivableRatioRouter.routes());
-    // app.use(financeRatioRouter.routes());
+    app.use(financeRatioRouter.routes());
     app.use(mainRouter.routes());
 }
---
+++ routers/financeRatioRouter.js
@@ -0,0 +1,65 @@
+const Router = require("koa-router");
+const querystring = require("querystring");
+let getData = require('./common');
+
+let router = new Router({
+    prefix:"/FinanceReceiveRatio"
+});
+
+router.get('/',async (ctx)=>{
+    // console.log(ctx.query.id);
+    let {id, userid, authid} = ctx.query;
+    let auth = {};
+    // console.log("authid",authid);
+    // console.log(typeof(authid))
+    if(typeof(authid) == "undefined") {
+        // console.log("first");
+        const authUrl = 'getUserAuth.jsp';
+        const postData1 = querystring.stringify({
+            'userid' : userid,
+            'menuID' : id
+        });
+        // console.log(postData1);
+        auth = await getData(authUrl, postData1);
+    } else {
+        // console.log("second");
+        auth = [{
+            'AUTH_TYPE': '0',
+            'AUTH_ID': authid
+        }];
+    }
+    // console.log("auth");
+    // console.log(auth);
+    
+    await ctx.render('FinanceReceiveRatio/index.pug',{auth});
+    
+});
+
+router.get('/getData', async (ctx) => {
+    // console.log("getData");
+    // console.log(ctx.query);
+    let {authType, authID} = ctx.query;
+    let postData = querystring.stringify({
+        'authType': authType,
+        'authID': authID,
+        'getType': 'now'
+    });
+    let postData1 = querystring.stringify({
+        'authType': authType,
+        'authID': authID,
+        'getType': 'lastMonth'
+    });
+    const getDataUrl = 'financeReceiveRatio/getByHat.jsp';
+    let nowData = await getData(getDataUrl, postData);
+    // console.log(postData);
+    let lastMonthData = await getData(getDataUrl, postData1);
+    // console.log(lastMonthData);
+
+    // let data = {
+    //     name: 'claire'
+    // }
+    ctx.body = {nowData,lastMonthData};
+});
+
+
+module.exports = router;
